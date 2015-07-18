8064abfe89a89ffdb19b7e234ddc27d34011d97b
Andrew Clissold <andrewclissold@gmail.com>
2015-07-18 12:15:25 -0400

    Exercise 3: use platform-agnostic line ending

--- three.js
+++ three.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
+var os = require('os');
 
 var buf = fs.readFileSync(process.argv[2]);
 var str = buf.toString();
-var lines = str.split('\n');
+var lines = str.split(os.EOL);
 
 console.log(lines.length-1); // -1 for trailing newline
